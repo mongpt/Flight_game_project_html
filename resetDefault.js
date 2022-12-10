80553cfe4db88ef183c1c10dd501f84611839893
mongpt <mongphantan@gmail.com>
2022-12-10 19:41:14 +0200

    ok

--- resetDefault.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('reset').addEventListener("click", resetDefault);
-function resetDefault() {
-    fetch('http://127.0.0.1:5000/resetDefault');
-    alert("User results have been reset to the default value");
-}
