40de8676eded92641a95c6cd42e0afb3774ce34a
Mong Phan <102502836+mongpt@users.noreply.github.com>
2022-12-17 19:57:07 +0200

    Change to use pythonanywhere server

--- Back_End.py
+++ Back_End.py
@@ -1,3 +1,4 @@
+'''
 # Heejin - top5/GET and newgamescore/POST
 import requests
 from flask import Flask, request
@@ -53,8 +54,59 @@ def weather(cityId):
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
+'''
+
+# FOR PYTHONANYWHERE server
+# Heejin - top5/GET and newgamescore/POST
+import requests
+from flask import Flask, request
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config["DEBUG"] = True
+import mysql.connector
+connection = mysql.connector.connect(
+         host='mongpt.mysql.pythonanywhere-services.com',
+         #port= 3306,
+         database='mongpt$flight_game',
+         user='mongpt',
+         password='xxxxxx',
+         autocommit=True
+)
+
+@app.route('/top5Players', methods = ['GET', 'POST'])
+def top5Players():
+    if request.method == 'GET':
+        sql = "SELECT name, points FROM users order by points desc limit 5;"  #period1 (flight game db > users table)
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return json.dumps(result)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        sql = f"INSERT INTO users (name, points) VALUES (\"{data['username']}\",  \"{data['score']}\");"
+        cursor = connection.cursor()
+        cursor.execute( sql )
+        result = cursor.fetchall()
+        return json.dumps(result)
+
+@app.route('/resetDefault')
+def resetPlayerPoints():
+    myCursor = connection.cursor()
+    myCursor.execute("drop table if exists users;")
+    myCursor.execute("create table users(name varchar(10),points float);")
+    myCursor.execute("insert into users(name, points)values('A', 5),"
+                     "('B', 4),('C', 3),('D', 2),('E', 1);")
+    result = myCursor.fetchall()
+    return json.dumps(result)
 
 
-
-
-
+@app.route('/<cityId>')
+def weather(cityId):
+    complete_api_link = f"https://api.openweathermap.org/data/2.5/weather?id={cityId}&appid=b318472dc4571e2480bc555a091e5bb6"
+    api_link = requests.get(complete_api_link).json()
+    return json.dumps(api_link)
